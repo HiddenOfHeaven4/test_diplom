e743cd4173a51ab7d83e17986e8dcf085fedc344
HiddenOfHeaven4 <102412741+HiddenOfHeaven4@users.noreply.github.com>
2022-05-29 13:29:13 +0300

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { SHOP_ROUTE } from '../utils/consts'
 
+// Подвал приложения с применением state manager (MobX)
+
 export default observer(() => {
     return (
         <Container>
