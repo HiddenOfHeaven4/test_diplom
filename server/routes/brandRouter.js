560a6de94414499be3258f27bafd3389f09d1cc5
HiddenOfHeaven4 <102412741+HiddenOfHeaven4@users.noreply.github.com>
2022-05-29 13:03:52 +0300

    Update brandRouter.js

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,3 +1,5 @@
+// Маршрутизация приложения в компоненте бренда
+
 const Router = require('express');
 const router = new Router();
 const brandController = require('./../controllers/brandController');
