3cfcb2d32dac9523cdf38ee7ad4984e5eaa58e04
HiddenOfHeaven4 <102412741+HiddenOfHeaven4@users.noreply.github.com>
2022-05-29 13:51:46 +0300

    Update userApi.js

--- client/src/http/userApi.js
+++ client/src/http/userApi.js
@@ -1,6 +1,8 @@
 import {$authHost, $host} from "./index";
 import jwt_decode from "jwt-decode";
 
+// указание точного функционала методов при работе с авторизацией и регистрацией пользователя
+
 export const registration = async (email, password) => {
     const {data} = await $host.post('api/user/registration', {email, password, role: 'ADMIN'});
     localStorage.setItem('token', data.token);
